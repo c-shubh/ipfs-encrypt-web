56fc99ab4663a82c54589cd60fff1055fb4bced1
Shubh Chudasama <shubh.cs@proton.me>
2023-02-17 22:27:34 +0530

    Add tailwind css

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import { Button } from "@chakra-ui/react";
 export default function Home() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button colorScheme="blue">Button</Button>
     </div>
   );
