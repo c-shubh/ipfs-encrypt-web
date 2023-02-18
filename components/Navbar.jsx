361d5cfc2c3d058d831de8fa7ff94a5abdfb87fa
Shubh Chudasama <shubh.cs@proton.me>
2023-02-18 16:16:51 +0530

    Add a navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Box, Button, Image, Link, Stack, Text } from "@chakra-ui/react";
+export default function Navbar() {
+  return (
+    <Stack
+      bg={"blackAlpha.500"}
+      padding={"4"}
+      paddingX={"8"}
+      boxShadow={"outline"}
+      direction={"row"}
+      justifyContent={"space-between"}
+    >
+      <NavbarLogo />
+      <Stack direction={"row"} alignItems={"center"} spacing={"4"}>
+        <Link href="#">Home</Link>
+        <Link href="#">Section 1</Link>
+        <Link href="#">Section 2</Link>
+        <Link href="#">Section 3</Link>
+        <Button colorScheme="blue" size={"sm"}>
+          Login
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
+
+function NavbarLogo() {
+  return (
+    <Stack direction={"row"} alignItems={"center"}>
+      <Box boxSize={"12"}>
+        <Image src="cube.svg" alt="Logo" />
+      </Box>
+      <Text>IPFS Encrypt</Text>
+    </Stack>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Box>
+      <Navbar />
+    </Box>
   );
 }
