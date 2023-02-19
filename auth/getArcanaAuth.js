355bbc926c2bff5c56ff5c2ed2a1360bd2c88d36
0xvikasrushi <0xvikas@gmail.com>
2023-02-19 19:15:23 +0530

    env

--- auth/getArcanaAuth.js
+++ auth/getArcanaAuth.js
@@ -1,8 +1,9 @@
 import { AuthProvider } from "@arcana/auth";
 
-const auth = new AuthProvider(process.env.NEXT_PUBLIC_AUTHTOKENN, {
+const auth = new AuthProvider(process.env.NEXT_PUBLIC_AUTHTOKEN, {
   theme: "light",
 });
+// console.log("wo" + process.env.NEXT_PUBLIC_AUTHTOKEN);
 
 const getAuth = () => {
   return auth;
